35e960a52b093979e2c1c2ae48dbd967943544a6
Pablo Salinas <pez256@live.com.mx>
2019-09-26 17:07:37 -0500

    Envío de parámetros por GET en la URL

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) =>{
+let arr = [];
+let pers = {};
+
+/*app.get("/", (req, res) =>{
     res.send("Hola mundo");
-});
+});*/
 
 app.listen(3000, () => {
     console.log("Server is running...");
+});
+
+/*app.get("/:id", (req, res) =>{
+    console.log(req.params.id);
+    arr.push(req.params.id);
+    console.log(arr);
+});*/
+
+app.get("/:nombre/:edad", (req, res) =>{
+    console.log(req.params.nombre);
+    pers = {
+        "Nombre": req.params.nombre,
+        "Edad": req.params.edad 
+    };
+    console.log(pers);
 });
